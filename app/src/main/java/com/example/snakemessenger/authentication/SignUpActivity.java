8511c93e51a81f44a965285c88e170f9b1322932
Florin Mihalache <florinrm@users.noreply.github.com>
2022-04-26 23:45:14 +0300

    fix: changed findViewById with bindings in SignInActivity
    Added refactoring and converted to Kotlin - SignInActivity

--- app/src/main/java/com/example/snakemessenger/authentication/SignUpActivity.java
+++ app/src/main/java/com/example/snakemessenger/authentication/SignUpActivity.java
@@ -150,6 +150,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == Constants.REQUEST_IMAGE_CAPTURE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
